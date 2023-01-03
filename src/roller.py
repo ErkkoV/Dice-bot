8c59ce811c59d08867cd087d25ca46579451b45a
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-03 11:03:07 +0200

    roller basics

--- src/roller.py
+++ src/roller.py
@@ -1,3 +1,9 @@
 def roller(content):
-    print(content)
-    return "test"
+    try:
+        if content[6].lower() == "h" or content[6].lower() == "r":
+            return "roll1"
+
+        elif content[5]:
+            return "roll2"
+    except:
+        return "Insert roll as either: 4d + 5 or 4n + 5 or r3t2 + 3 or h3o2 + 5"
