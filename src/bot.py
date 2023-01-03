27280b311ee9994a028108ecd821240dbfd99ad9
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-03 10:06:04 +0200

    working messages

--- src/bot.py
+++ src/bot.py
@@ -8,18 +8,23 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Logged on as {self.user}!")
-
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
-
-
 intents = discord.Intents.default()
 intents.message_content = True
+client = discord.Client(intents=intents)
 
-client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("Kahvi"):
+        await message.channel.send("ToniPal Kahville!")
+
+
 client.run(DISCORD_TOKEN, log_handler=handler)
